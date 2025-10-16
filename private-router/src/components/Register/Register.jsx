1527b4f3afa674721e1c80cd5c3776f3f03f8fb8
Roksana Dilshad <tanvirhossaintufa@gmail.com>
2025-10-16 16:30:02 +0600

    add login register  form

--- private-router/src/components/Register/Register.jsx
+++ private-router/src/components/Register/Register.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const email = event.target.email.value
+        const password = event.target.password.value
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            this is the register page
-        </div>
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="card bg-base-100 w-full min-w-md shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className='font-bold text-3xl text-center'>Register Now!</h1>
+        <form onSubmit={handleSubmit}>
+            <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input name='email' type="email" className="input w-full" placeholder="Email" />
+          <label className="label">Password</label>
+          <input name='password' type="password" className="input w-full" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+        </form>
+        <h3>Already have an Account? <Link className='text-blue-700' to={'/login'}>Login</Link> now</h3>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
