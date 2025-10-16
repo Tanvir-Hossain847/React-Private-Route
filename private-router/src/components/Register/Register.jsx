d6e222478a4ff8711cb0f190f3ad79d647fa63fa
Roksana Dilshad <tanvirhossaintufa@gmail.com>
2025-10-16 21:42:33 +0600

    function of getting a user info and logging it

--- private-router/src/components/Register/Register.jsx
+++ private-router/src/components/Register/Register.jsx
@@ -1,27 +1,41 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import React, { use} from 'react';
 import { Link } from 'react-router';
-import { auth } from '../../firebase/firebase.init';
+import { AuthContext } from '../../context/AuthContext';
 
 const Register = () => {
 
-    const [submit, setSubmit] = useState()
+
+    const {createUser} = use(AuthContext)
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const email = event.target.email.value
         const password = event.target.password.value
-        console.log(email, password);
 
-        createUserWithEmailAndPassword(auth, email, password)
-                .then(result => {
-                    setSubmit(result
-                    );
-                })
-                .catch(error => {
-                    console.log(error);
-                })
+        createUser(email, password)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault()
+    //     const email = event.target.email.value
+    //     const password = event.target.password.value
+    //     console.log(email, password);
+
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //             .then(result => {
+    //                 setSubmit(result
+    //                 );
+    //             })
+    //             .catch(error => {
+    //                 console.log(error);
+    //             })
+    // }
 
     return (
        <div className="hero bg-gray-700 min-h-screen">
@@ -38,7 +52,7 @@ const Register = () => {
                 <div><a className="link link-hover text-white">Forgot password?</a></div>
                 <button className="btn bg-white text-black btn-neutral mt-4">Register</button>
               </fieldset>
-              <h3 className='text-center text-green-500'>{submit ? "Registration Complete" : ''}</h3>
+              <h3 className='text-center text-green-500'>''</h3>
               </form>
               <h3 className='text-white'>Already have an Account? <Link className='text-blue-700' to={'/register'}>Login</Link> now</h3>
             </div>
--- private-router/src/context/AuthProvider.jsx
+++ private-router/src/context/AuthProvider.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const authinfo = {
-        user : 'kola@mola.com'
+        createUser,
     }
     return (
         <AuthContext value={authinfo}>
