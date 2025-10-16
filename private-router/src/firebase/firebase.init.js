5f2a20d813d27098c8e4f7148565b275a78d957d
Roksana Dilshad <tanvirhossaintufa@gmail.com>
2025-10-16 13:29:26 +0600

    add base file daisy tailwind and firebase

---
+++ private-router/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD313SYw1CSPzIXFWFFcq1M7nnxTAm4VmQ",
+  authDomain: "react-private-route-c3276.firebaseapp.com",
+  projectId: "react-private-route-c3276",
+  storageBucket: "react-private-route-c3276.firebasestorage.app",
+  messagingSenderId: "955648598671",
+  appId: "1:955648598671:web:bc75177c1405a6c2e506fa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
